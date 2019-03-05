d1b75dfb86dc401195d6a796f4abe16cdbbbb677
hoang <a2k40pbc@gmail.com>
2019-03-05 13:00:39 +0700

    add setting.py

--- setting.py
+++ setting.py
@@ -1,4 +1,15 @@
 # fixed file paths
 
-ACTION_SET = ['actionsets/ActionSet1.txt', 'actionsets/ActionSet2.txt', 'actionsets/ActionSet3.txt']
-DATA_PATH = 'data/'
+ACTION_SET  = ['actionsets/ActionSet1.txt', 'actionsets/ActionSet2.txt', 'actionsets/ActionSet3.txt']
+DATA_PATH   = 'data/'
+ACTION_NAME = ['Wave','Hammer','Smash','Catch','Forward Punch','Throw','Draw X','Draw Tick','Draw ',
+               'Circle','Clapping Hand','Two hand Wave','Side Boxing','Bend','Forward Kick','Side Kick','Jogging',
+               'Tennis Swing','Tennis Serve','Golf Swing','Pickup&throw']
+
+NUMBER_OF_MOST_JOINTS = 3
+NUMBER_OF_JOINTS = 20
+NUMBER_OF_ACTIONS = 20
+NUMBER_OF_LEVELS = 3
+OVERLAP = True
+TIMEVAR = False
+
--- utils.py
+++ utils.py
@@ -1,10 +1,9 @@
 import os
 import numpy as np
-import setting
 
 def getAllDataFilename(filepath):
     allFile = []
-    for file in os.listdir(setting.DATA_PATH):
+    for file in os.listdir(filepath):
         if file.endswith(".txt"):
             allFile.append(file)
     return allFile
@@ -21,3 +20,18 @@ def getActionSubject(filename):
     action_labels = [int(x.strip()[5:7]) for x in action_labels]
     return action_labels
 
+# sizeMatrix = N
+# get index of upper Covariance Matrix
+
+def getValueMatrix(sizeMatrix):
+    getMatrixIndex = np.zeros(0)
+    tmp = sizeMatrix
+    for i in range(sizeMatrix):
+        getMatrixIndex = np.append(getMatrixIndex, np.arange(tmp-sizeMatrix+i, tmp))
+        tmp += sizeMatrix
+    getMatrixIndex = getMatrixIndex.astype(np.uint8)
+    return getMatrixIndex
+
+def getIdxMostJoints(matrixJoints):
+
+    return None
